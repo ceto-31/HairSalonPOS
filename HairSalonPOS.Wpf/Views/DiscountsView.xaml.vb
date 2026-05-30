0114a4888c12f02e1237222ff59daaf721c98306
ceto-31 <anicetoakaajobina@gmail.com>
2026-05-30 15:38:48 +0800

    add discounts view

---
+++ HairSalonPOS.Wpf/Views/DiscountsView.xaml.vb
@@ -0,0 +1,8 @@
+Namespace Views
+    Partial Public Class DiscountsView
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
