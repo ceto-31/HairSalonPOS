25d75f3388862e564779d431c3646e4632589859
ceto-31 <anicetoakaajobina@gmail.com>
2026-05-30 15:38:48 +0800

    add appointments view

---
+++ HairSalonPOS.Wpf/Views/AppointmentsView.xaml.vb
@@ -0,0 +1,8 @@
+Namespace Views
+    Partial Public Class AppointmentsView
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
