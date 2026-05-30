f1adec9bb915d5622b1b730f5b9ebf4ec019d59b
ceto-31 <anicetoakaajobina@gmail.com>
2026-05-30 15:38:49 +0800

    add settings view

---
+++ HairSalonPOS.Wpf/Views/SettingsView.xaml.vb
@@ -0,0 +1,8 @@
+Namespace Views
+    Partial Public Class SettingsView
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
