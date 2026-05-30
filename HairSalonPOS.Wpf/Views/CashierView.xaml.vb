006156eb621da5073a0de163160d91301c840974
ceto-31 <anicetoakaajobina@gmail.com>
2026-05-30 15:38:47 +0800

    add cashier view

---
+++ HairSalonPOS.Wpf/Views/CashierView.xaml.vb
@@ -0,0 +1,9 @@
+Namespace Views
+    Partial Public Class CashierView
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
