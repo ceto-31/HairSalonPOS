fe3d3403b5499e9e3df084a9447fd73fb9566a77
ceto-31 <anicetoakaajobina@gmail.com>
2026-05-30 15:38:49 +0800

    add staff view

---
+++ HairSalonPOS.Wpf/Views/StaffView.xaml.vb
@@ -0,0 +1,8 @@
+Namespace Views
+    Partial Public Class StaffView
+        Inherits UserControl
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
